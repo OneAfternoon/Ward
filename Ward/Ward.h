b37fcc62dacb77733d8bdba15a2ffd1fcdb1107a
Swain Molster <swain.molster@gmail.com>
2018-08-16 23:04:56 -0400

    Initial Commit

---
+++ Ward/Ward.h
@@ -0,0 +1,19 @@
+//
+//  Ward.h
+//  Ward
+//
+//  Created by Swain Molster on 8/16/18.
+//  Copyright © 2018 Swain Molster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ward.
+FOUNDATION_EXPORT double WardVersionNumber;
+
+//! Project version string for Ward.
+FOUNDATION_EXPORT const unsigned char WardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ward/PublicHeader.h>
+
+
